a80093a337bf04b73d0dc74f68afee27cccb9e17
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-06-08 03:19:26 +0530

    Add login button components.

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { LoginButton } from "./src/login/login";
 
 export default function App() {
+
 	return (
-		<View style={{padding: '20%'}}>
-			<Text style={styles.CustomButton}>Button</Text>	
+		<View>
+			<LoginButton title="Hello World" buttonStyle="normal1" />
 		</View>
 	);
+	
 }
-
-const styles = StyleSheet.create({
-	CustomButton: {
-		width: '80%',
-		padding: '3%',
-		backgroundColor: 'blue',
-		color: 'white',
-		textAlign: 'center',
-		fontFamily: 'Roboto-Black',
-		fontWeight: '500'
-	}
-});
