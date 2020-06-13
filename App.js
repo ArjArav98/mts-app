7fc059bbfbfb4fc57b0bbe79f3af91ca5a7708a9
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-06-13 18:08:10 +0530

    Finish the Payment Successful Screen. Add images for comments submission ratings.

--- App.js
+++ App.js
@@ -36,13 +36,61 @@ const Navbar = StackNavigator({
 	},
 	Payment: {
 		screen: PaymentHome
-	}
+	},
+	SucessfulPayment: {
+		screen: PaymentSuccessfulHome
+	},
 }, {
 	initialRouteName: 'Login',
 	defaultNavigationOptions: {
 		header: null
 	}
 });
+
+function PaymentSuccessfulHome(props){
+
+	let navigationOptions = {
+		header: null
+	};
+
+	let { navigate } = props.navigation;
+	
+	return (
+		<View style={homeStyles.HomeContainer}>
+			
+			<SectionHeader title="PAYMENT SUCCESSFUL" style={{width: '100%'}}/>
+			<View style={[homeStyles.HomeElemContainer,{flex: 0.5}]}>
+				<Text style={homeStyles.HomeElemText}>Thanks for shopping with us!</Text>
+				<Text style={homeStyles.HomeElemText}>
+					Your total bill amount is Rs 465. The number of items is 5.
+				</Text>
+			</View>
+			<View style={[homeStyles.HomeElemContainer, {justifyContent: 'flex-start'}]}>
+				<View style={{width: '100%', flexDirection: 'row', justifyContent: 'center'}}>
+					<Image 	source={require('./assets/images/star.png')} 
+							style={{width: 60, height: 60, marginLeft: '1%', marginRight: '1%'}} />
+					<Image 	source={require('./assets/images/star.png')} 
+							style={{width: 60, height: 60, marginLeft: '1%', marginRight: '1%'}} />
+					<Image 	source={require('./assets/images/star.png')} 
+							style={{width: 60, height: 60, marginLeft: '1%', marginRight: '1%'}} />
+					<Image 	source={require('./assets/images/star.png')} 
+							style={{width: 60, height: 60, marginLeft: '1%', marginRight: '1%'}} />
+					<Image 	source={require('./assets/images/star.png')} 
+							style={{width: 60, height: 60, marginLeft: '1%', marginRight: '1%'}} />
+				</View>
+				<LoginInput placeholder="Your Comments" style={{width: '80%', marginTop: '5%'}} />
+				<LoginButton 	title="SUBMIT" buttonStyle="default" style={{width: '80%', marginTop: '5%'}}
+							 	navigate={navigate} navigateScreen={'AppHome'} />
+			</View>
+
+		</View>
+	);
+
+}
+
+/****************/
+/* PAYMENT HOME */
+/****************/
 
 function PaymentHome(props){
 
@@ -61,7 +109,8 @@ function PaymentHome(props){
 				<Text style={homeStyles.HomeElemText}>
 					Please show this code at the billing counter to make your payment.
 				</Text>
-				<TouchableOpacity style={homeStyles.HomeElemImageContainer} onPress={()=>navigate('Barcode')}>
+				<TouchableOpacity 	style={homeStyles.HomeElemImageContainer}
+									onPress={()=>navigate('SucessfulPayment')}>
 					<Image 	style={homeStyles.HomeElemImage} 
 							source={require('./assets/images/qr.png')} />
 				</TouchableOpacity>
